88b9a830f1854b9e7a26de61ba7000450fa26454
Ben Jones <18bjones@thelangton.org.uk>
2025-01-17 12:29:22 +0000

    bmp test

--- test.cpp
+++ test.cpp
@@ -3,23 +3,43 @@
 #define WIDTH 800
 #define HEIGHT 600
 
+enum piece {
+    I, 
+    O, 
+    T, 
+    S, 
+    Z, 
+    J, 
+    L
+};
+
+struct pip {
+    int x; // position starts from top left at 0,0
+    int y;
+    enum piece p;
+};
+
+int board[20][10]; 
+
+void draw_square(int x, int y)
+
 int main( int argc, char** argv )
 {
+    //Driver code
     SDL_Init( SDL_INIT_EVERYTHING );
     SDL_Window* window = SDL_CreateWindow("SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN );
     SDL_Renderer* renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
-
-    const std::vector< SDL_Vertex > verts = {
-        { SDL_FPoint{ 400, 150 }, SDL_Color{ 255, 0, 0, 255 }, SDL_FPoint{ 0 }, },
-        { SDL_FPoint{ 200, 450 }, SDL_Color{ 0, 0, 255, 255 }, SDL_FPoint{ 0 }, },
-        { SDL_FPoint{ 600, 450 }, SDL_Color{ 0, 255, 0, 255 }, SDL_FPoint{ 0 }, },
-    };
 
     SDL_Rect tr;
     tr.x = 10;
     tr.y = 10;
     tr.w = 100;
     tr.h = 100;
+
+    	
+    SDL_Surface * image = SDL_LoadBMP("image.bmp");
+    SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, image);
+
 
     bool running = true;
     while( running )
@@ -29,29 +49,23 @@ int main( int argc, char** argv )
         {
             if( ( SDL_QUIT == ev.type ) ||
                 ( SDL_KEYDOWN == ev.type && SDL_SCANCODE_ESCAPE == ev.key.keysym.scancode ) ) // exit on escape
-            {
-                running = false;
-                break; 
-            }
-        }
+            {running = false;  break; }        }
 
-        /*
-        SDL_SetRenderDrawColor( renderer, 0, 0, 0, SDL_ALPHA_OPAQUE );
-        SDL_RenderClear( renderer );
-        SDL_RenderGeometry( renderer, nullptr, verts.data(), verts.size(), nullptr, 0 );
-        SDL_RenderPresent( renderer );
-        */
+    
+
 
        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE );
        SDL_RenderClear(renderer);
-       SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE );
-       SDL_RenderDrawRect(renderer, &tr);
+       //SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE );
+       //SDL_RenderDrawRect(renderer, &tr);
+       SDL_RenderCopy(renderer, texture, NULL, NULL);
+       
        SDL_RenderPresent(renderer);
        SDL_Delay(16);
-       tr.x += 1;
-
+       //tr.x += 1;
     }
-
+    SDL_DestroyTexture(texture);
+    SDL_FreeSurface(image);
     SDL_DestroyRenderer( renderer );
     SDL_DestroyWindow( window );
     SDL_Quit();
