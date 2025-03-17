96725306b1358e7c129baf61cb6978e6267795ae
Ben Jones <18bjones@thelangton.org.uk>
2025-03-17 00:46:22 +0000

    Finished rotate.cpp

--- temp/rotate.cpp
+++ temp/rotate.cpp
@@ -1,112 +1,102 @@
 #include <iostream>
 
-void transpose(int* index, int len){
+void transpose(int* fpntr, int len){
+    // Transposes the matrix (swaps elements about the centre)
     int swap;
     for(int i = 0; i < len; i++){
         for(int j = i; j < len; j++){
             if(i==j) {
-                continue;
+                continue;                                       // ignore central line
             }
             else{
-                swap = *(index + (len*j)+ i);
-                *(index + (len*j)+ i) = *(index + (len*i)+ j);
-                *(index + (len*i)+ j) = swap;
+                swap = *(fpntr + (len*j)+ i);
+                *(fpntr + (len*j)+ i) = *(fpntr + (len*i)+ j);
+                *(fpntr + (len*i)+ j) = swap;
             }
         }
     }
 }
 
-void r1(int* index, int){
-    int len = 4;
+void reverse(int* fpntr, int len, bool ccw){
+    // Reverses either the rows or columns of a matrix
     int swap;
-    printf("%p", index);
-    printf("\n");
-    for(int i; i < len; i++){
-                printf("ff\n");
-                for(int j = 0; j < (len)/2; j++){
-                    swap = *(index + (len - j) + (len * i));
-                    //*(index + (len - j) + (len * i)) = *(index+j+(len*i));
-                    *(index+j+(len*i)) = swap;
-                }    
-            }
-
-}
-
-void reverse(int* index, int len, bool dir){
-    int swap;
-    if(dir == true){           // Reverse the columns
-        if(len % 2 == 1) {
-            for(int j = 0; j < (len-1)/2; j++){
-                swap = *(index + len - j);
-                *(index + len - j) = *(index+j);
-                *(index+j) = swap;
+    if(ccw == 1){                                               // Revese the columns
+        if(len % 2 == 1){
+            for(int i = 0; i < (len-1)/2; i++){                 // ignore middle row
+                for(int j = 0; j < len; j++){
+                    swap = *(fpntr+j+((len*len)-(len*(i+1))));  //for a 4x4: f(0) = 12, f(1) = 8, thus f(i) = l^2-l(i+1) 
+                    *(fpntr+j+((len*len)-(len*(i+1)))) = *(fpntr+j+(len*i));
+                    *(fpntr+j+(len*i)) = swap;
                 }
+            }
         }else{
-            for(int i; i < 4; i++){
-                printf("break");
-                for(int j = 0; j < (4)/2; j++){
-                    swap = *(index + (len - j) + (len * i));
-                    *(index + (len - j) + (len * i)) = *(index+j+(len*i));
-                    *(index+j+(len*i)) = swap;
+            for(int i = 0; i < len/2; i++){
+                for(int j = 0; j < len; j++){
+                    swap = *(fpntr+j+((len*len)-(len*(i+1))));  // same as above
+                    *(fpntr+j+((len*len)-(len*(i+1)))) = *(fpntr+j+(len*i));
+                    *(fpntr+j+(len*i)) = swap;
+                }
+            }
+        }
+    }
+    else{                                                       // Reverse the rows
+        if(len % 2 == 1){
+            for(int i = 0; i < len; i++){
+                for(int j = 0; j < ((len-1)/2)+1; j++){         // ignore middle column
+                    swap = *(fpntr+(len-j-1)+(i*len));
+                    *(fpntr+(len-j-1)+(i*len)) = *(fpntr+j+(len*i));
+                    *(fpntr+j+(len*i)) = swap;
+                }
+            }
+        }
+        else{
+            for(int i = 0; i < len; i++){
+                for(int j = 0; j < (len)/2; j++){   
+                    swap = *(fpntr+(len-j-1)+(i*len));
+                    *(fpntr+(len-j-1)+(i*len)) = *(fpntr+j+(len*i));
+                    *(fpntr+j+(len*i)) = swap;
                 }    
             }
         }
-    }else{                  // Reverse the rows
-    return;
     }
 }
 
-/*void rotate(int* index, bool dir){
-    // 1 is clockwise 90, 0 is counterclockwise
-    transpose(int* index)
-    reverse(int* index, bool dir)    
-}*/
-
-void seg(int* scp){
-    *(scp+1) = 5;
+void rotate(int* fpntr, int len, bool ccw){
+    // fpntr - front pointer, an int* to the first element [0][0]
+    // len - length, the side length of the n x n matrix as an integer
+    // ccw - counterclockwise, if 1 will rotate counterclockwise
+    transpose(fpntr, len);
+    reverse(fpntr, len, ccw);    
 }
 
 int main(){
-    int sc[5];
-    int * scp = &sc[0];
-    printf("alive\n");
-    seg(scp);
-    printf("%d", sc[1]);
-}
-
-/*
-int main(){
-    int tst[4][4] = {
-        {11,12,13,14},
-        {21,22,23,24},
-        {31,32,33,34},
-        {41,42,43,44}
+    int matrix[4][4] = {    //      Should become:
+        {11,12,13,14},      //      41 31 21 11
+        {21,22,23,24},      //      42 32 22 12
+        {31,32,33,34},      //      43 33 23 13
+        {41,42,43,44}       //      44 34 24 14
     };
     int len = 4;
-    int* tptr = &tst[0][0];
+    int* mptr = &matrix[0][0];
 
-    
-        printf("orig. Matrix:");
-        printf("\n\n");
-
-    for (int i = 0; i < 4; i++){
-        for (int j = 0; j < 4; j++){
-            printf("%d", tst[i][j]);
+    printf("orig. Matrix:\n");
+    for (int i = 0; i < len; i++){
+        for (int j = 0; j < len; j++){
+            printf("%d", matrix[i][j]);
             printf(", ");
         }
         printf("\n");
     }
     printf("\n");
-    printf("Altered Matrix:");
-    printf("\n\n");
-    transpose(tptr, 4);
-    r1(tptr, 4);
+    
+    rotate(mptr, len, 0);   // try 1 for counterclockwise
 
-    for (int i = 0; i < 4; i++){
-        for (int j = 0; j < 4; j++){
-            printf("%d", tst[i][j]);
+    printf("\nAltered Matrix:\n");
+    for (int i = 0; i < len; i++){
+        for (int j = 0; j < len; j++){
+            printf("%d", matrix[i][j]);
             printf(", ");
         }
         printf("\n");
     }
-}*/
+}
