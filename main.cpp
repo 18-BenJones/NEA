68e591caf6780075ff9b1ac18866ee309a4f8c43
Ben Jones <18bjones@thelangton.org.uk>
2025-03-20 16:10:20 +0000

    expanded movepiece(), added single drop ctrl

--- main.cpp
+++ main.cpp
@@ -1,42 +1,30 @@
 #include<SDL2/SDL.h>            // for SDL 
 #include<stdlib.h>              // for rand()
-#include<time.h>                // to seed rand() with srand
+#include<time.h>                // to seed rand() with srand(time(NULL))
 
 /*
     TODO:
-    today: 
-    * finish tetris:
-        * stop the player from making illegal moves (rotates, out of bounds) - wishpos() function
-        * add the piece staticlly to the board after it reaches bottom - block bellow or EOB
-        * hard drop the piece
-        * add line clearing logic to update(5)
+    Now: finish tetris:
+    * finish wishpos()
+    * add line clearing logic to update(5)
+    * finish gamestate struct (implement scoring, menu, gamespeed)
+    * add basic menu system 
         
-        * req functions: extremity?() mcpstatc() distancebelow() wishpos()
-        
-    tmrw:
-    * add score and change gms to the struct
-    * add some boilerplate states for starting and repeating a game
-    * if time allows - add ai functionality
-        * create weighting function
-        * movespace culler
-        * genetic algo
-        * threads and batch proccessing
-        * run a simulation (probably overnight)
-    * If not - start write up on reduced scope 
-         
-    
-    Friday:
-    * do write up
+    later:
+    * either:
+    * make the clone better (fancy blocks, sounds, upcoming pieces, settings, hold function, psuedorandomness)
+    * or write the AI (still need pseudorandomness)
+    * write up
 */
 
 // ------ global variables ------ 
 
-SDL_Window* window;             // SDL object that contains the functionality of the window
-SDL_Renderer* renderer;         // SDL object that contains OpenGL functionality
-bool running;                   // Track the state of the core game loop, global bcus needs to change regardless of scope
+SDL_Window* window;                             // SDL object that contains the functionality of the window
+SDL_Renderer* renderer;                         // SDL object that contains OpenGL functionality
+bool running;                                   // Track the state of the core game loop, global bcus needs to change regardless of scope
 int WIDTH = 800;
 int HEIGHT = 600;
-int BLOCKLEN = HEIGHT/20;       // uses height as the constraint to get 20 equal blocks on the screen
+int BLOCKLEN = HEIGHT/20;                       // uses height as the constraint to get 20 equal blocks on the screen
 int board[20][10] = {
 {0,0,0,0,0,0,0,0,0,0},
 {0,0,0,0,0,0,0,0,0,0},
@@ -59,23 +47,23 @@ int board[20][10] = {
 {0,0,0,0,0,0,0,0,0,0},
 {0,0,0,0,0,0,0,0,0,0} };
 
-struct gamestate {              // UNUSED aggrigate all the data about the games state into a single struct
+struct gamestate {                              // UNUSED aggrigate all the data about the games state into a single struct
     bool running;
     int ticklen;
     int score;
 };
 
-struct boardpos {               // a struct used to encapsulate an x - y board position of various things
+struct boardpos {                               // a struct used to encapsulate an x - y board position of various things
     int x;
     int y;
 };
 
-struct cur {                    // Structure that encapsulates the position, type, rotation and colour of the current piece
+struct cur {                                    // Structure that encapsulates the position, type, rotation and colour of the current piece
     boardpos pos;
     int piece[4][4]; 
 };
 
-const Uint8 colour[7][3] {      // gives a colour based of the value stored by the shape matricies 
+const Uint8 colour[7][3] {                      // gives a colour based of the value stored by the shape matricies 
     {65, 230, 255},     // light blue
     {0, 0, 255},        // Dark blue,
     {255, 165, 65},     // orange
@@ -85,7 +73,7 @@ const Uint8 colour[7][3] {      // gives a colour based of the value stored by t
     {255, 0, 0}         // red
 };
 
-const int shape[7][4][4] = {    // Stores the shapes as a 3D array, using the values of the numbers to store colour
+const int shape[7][4][4] = {                    // Stores the shapes as a 3D array, using the values of the numbers to store colour
     {           // I
                 {0,1,0,0},
                 {0,1,0,0},
@@ -130,7 +118,7 @@ const int shape[7][4][4] = {    // Stores the shapes as a 3D array, using the va
     }
 };
 
-cur current = {3, 0,            // defualt block spawn position, 3 is actually 4th space
+cur current = {3, -1,                           // defualt block spawn position, 3 is actually 4th space, -1 because it moves down once on spawn
    {{0,0,0,0},                  // init matrix that gets overridden by the first piece
     {0,0,0,0},
     {0,0,0,0},
@@ -139,7 +127,7 @@ cur current = {3, 0,            // defualt block spawn position, 3 is actually 4
 
 // ------ Logic functions ------
 
-void transpose(int* fpntr, int len){    // Transposes the matrix (swaps elements about the diagonal centre line)
+void transpose(int* fpntr, int len){            // Transposes the matrix (swaps elements about the diagonal centre line)
     int swap;
     for(int i = 0; i < len; i++){
         for(int j = i; j < len; j++){
@@ -213,39 +201,98 @@ void changepeice(){                             // sets current.piece to a rando
     }
 }
 
-void movepiece(int dir){                               // Main function for moving the current peice through board[][]
-    for(int i = 0; i < 4; i++){
+bool wishpos(boardpos pos){                     // return true if the desired position is valid. defined here because its used in void functions below it 
+    
+}
+
+void mcpstatic(){                               // Function to glue pieces that have hit the bottom to the board 
+    for(int i = 0; i < 4; i++){     
+        for(int j = 0; j < 4; j++){
+            if(current.piece[i][j] != 0){
+                board[current.pos.y + i][current.pos.x + j] = current.piece[i][j]; // replace the old piece where it was
+            }
+        }
+    }
+
+    current.pos.x = 3;
+    current.pos.y = 0;  // reset all of current         
+    changepeice();      // get a new piece 
+    
+    if(wishpos(current.pos)){       // draw the new piece 
+        for(int i = 0; i < 4; i++){     
+            for(int j = 0; j < 4; j++){
+                if(current.piece[i][j] != 0){
+                    board[current.pos.y + i][current.pos.x + j] = current.piece[i][j];
+                }
+            }
+        }
+    }else{                          // no space to draw the new peice; game over!
+        running = false;
+    }
+}
+
+void movepiece(int dir){                        // Main function for moving the current peice through board[][]
+    
+    int dist = 1; // used to track the distance dropped by the hard drop
+
+    for(int i = 0; i < 4; i++){                        // This is destructive so that the current piece doesn't affect wishpos() 
+        if(current.pos.y + i < 0){continue;}           // case for current.pos.y = -1
         for(int j = 0; j < 4; j++){
             if(current.piece[i][j] != 0){
                 board[current.pos.y + i][current.pos.x + j] = 0;
             }
         }
     }
+
     switch(dir){
         case 0: // down
+            if (wishpos({current.pos.x, current.pos.y + 1})){
             current.pos.y = current.pos.y + 1;
+            }else{
+                mcpstatic();  // this is the only function that 
+                return;       // get out movepiece() early as not to draw 2 peices      
+            }
         break;
         case 1: // left
-            current.pos.x = current.pos.x - 1;
-        break;
+            if(wishpos({current.pos.x - 1, current.pos.y})){
+                current.pos.x = current.pos.x - 1;
+                break;
+            }else{
+                break;
+            }
         case 2: // right
-            current.pos.x = current.pos.x + 1;
-        case 4:
-            rotate(&current.piece[0][0], 4, 0);
-        break;
+            if(wishpos({current.pos.x + 1, current.pos.y})){
+                current.pos.x = current.pos.x + 1;
+                break;
+            }else{
+                break;
+            }
+        case 3: // hard drop
+            while(wishpos({current.pos.x, current.pos.y + dist})){
+                dist = dist + 1;
+            }
+            current.pos.y = current.pos.y + dist;
+            break;
+        case 4: // rotate
+            rotate(&current.piece[0][0], 4, 0); // as rotate() operates on current.piece[][] rather than board[][], this is non-destructive
+            if(wishpos(current.pos)){
+                break;
+            }else{  // this if looks inverted because we need to change current back if the position was invalid
+                rotate(&current.piece[0][0], 4, 1); // rotate the matrix back 
+            }
+            break;
     }
-    for(int i = 0; i < 4; i++){
+
+    for(int i = 0; i < 4; i++){             // readd the piece to the board
         for(int j = 0; j < 4; j++){
             if(current.piece[i][j] != 0){
                 board[current.pos.y + i][current.pos.x + j] = current.piece[i][j];
             }
         }
     }
-
-
 }
 
-void lineclear(){
+void lineclear(){                               // UNFINISHED
     int zerocount = 10;
     int swap;
     for(int i = 0; i < 20; i++){
@@ -269,7 +316,7 @@ void lineclear(){
 }
 }
 
-boardpos cvrtss(boardpos ogpos){       // converts a boardpos into its screen space (pixel coordinate) equivilent 
+boardpos cvrtss(boardpos ogpos){                // converts a boardpos into its screen space (pixel coordinate) equivilent 
     boardpos screenspace;
     screenspace.x = ogpos.x * BLOCKLEN;
     screenspace.y = ogpos.y * BLOCKLEN;
@@ -279,10 +326,10 @@ boardpos cvrtss(boardpos ogpos){       // converts a boardpos into its screen sp
 // ------ Drawing functions ------
 
 void drawblock(boardpos pos, SDL_Color col){    // add a draw call to the render queue for a single block on the grid at a given board coordinate
-    pos = cvrtss(pos);
-    SDL_Rect square = {pos.x, pos.y, BLOCKLEN, BLOCKLEN};
-    SDL_SetRenderDrawColor(renderer, col.r, col.g, col.b, SDL_ALPHA_OPAQUE);
-    SDL_RenderFillRect(renderer, &square);
+    pos = cvrtss(pos);                                                          // find the pixel coords for the block
+    SDL_Rect square = {pos.x, pos.y, BLOCKLEN, BLOCKLEN};                       // defines the block to be drawn
+    SDL_SetRenderDrawColor(renderer, col.r, col.g, col.b, SDL_ALPHA_OPAQUE);    // set the colour
+    SDL_RenderFillRect(renderer, &square);                                      // draws a filled square of the given colour 
 }
 
 void drawgrid(){                                // Draws a white grid to differentiate the board from the background
@@ -304,11 +351,8 @@ void drawboard(){                               // takes the current state of th
     for(int i = 0; i < 20; i++){
         for(int j = 0; j < 10; j++){
             if(board[i][j] != 0){
-                tpos.x = j;
-                tpos.y = i;
-                tcol.r = colour[board[i][j]-1][0];
-                tcol.g = colour[board[i][j]-1][1];
-                tcol.b = colour[board[i][j]-1][2];
+                tpos = {j, i};  // grab the position of the block, below gets its colour
+                tcol = {colour[board[i][j]-1][0], colour[board[i][j]-1][1], colour[board[i][j]-1][2]};
                 drawblock(tpos, tcol);
             }
         }
@@ -329,21 +373,23 @@ void update(int action){                        // primary enclosing function fo
         case 2: // right 
                 movepiece(2);
             break;
-        case 3: // hard drop
-                // TODO implement this later
+        case 3: // down by 1
+                movepiece(0);   // gravity is the same as going down; abstraction
+        case 4: // hard drop
+                movepiece(3);
             break;
-        case 4: // rotate
+        case 5: // rotate the piece
             movepiece(4); 
             break;
-        case 5: // check for line clears
+        case 6: // check for line clears
             lineclear();
             break;
     }
 }
 
-void input(SDL_Event ev){                   // Handle keyboard input via SDL
+void input(SDL_Event ev){                       // Handle keyboard input via SDL
     if((SDL_QUIT == ev.type) || (SDL_KEYDOWN == ev.type && SDL_SCANCODE_ESCAPE == ev.key.keysym.scancode)) {
-    running = false;                        // exit on escape key
+    running = false;                        // exit on escape 
     }else{                                  
     switch(ev.type){
         case SDL_KEYUP:                     // the release of a key, slightly counterintuitivley
@@ -357,37 +403,21 @@ void input(SDL_Event ev){                   // Handle keyboard input via SDL
                 case SDLK_DOWN:
                     update(3);
                     break;
-                case SDLK_UP:
+                case SDLK_UP:    // hard drop
                     update(4);
                     break;
+                case SDLK_SPACE: // rotate
+                    update(5);
             }
         }
     }
 }
 
-void draw(){                                // Primary rendering function that handles adding all neccesry geometry to the renderer's queue
+void draw(){                                    // Primary rendering function that handles adding all neccesry geometry to the renderer's queue
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE); 
     SDL_RenderClear(renderer);      // Clear the renderer after each frame
     drawgrid();                     // grid first so it gets overdrawn by the blocks
-    drawboard();                   
-    SDL_RenderPresent(renderer);    // tell OpenGL to draw whats in the renders buffer
-}
-
-void testsquare(int flag){ // test square babeeee  
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE); 
-    SDL_RenderClear(renderer);      // Clear the renderer after each frame
-    drawgrid();    
-    drawboard();
-    if (flag == 1){
-        boardpos  bp = {12, 10};
-        SDL_Color c = {255,0,0};
-        drawblock(bp, c);
-    }                    
-    else{
-        boardpos  bp = {12, 10};
-        SDL_Color c = {0,0, 255};
-        drawblock(bp, c);
-    }                    
+    drawboard();                    // draw the all the blocks
     SDL_RenderPresent(renderer);    // tell OpenGL to draw whats in the renders buffer
 }
 
@@ -411,10 +441,8 @@ int main( int argc, char** argv ){
     int dtime = 0;
     
     srand(time(NULL));
-    changepeice(); // initialise a random piece
-    movepiece(0);
-
-
+    changepeice(); // initialise the first piece
+    movepiece(0);  // put the piece into board[][] and move it down 1
 
     while( running )
     { 
@@ -423,13 +451,12 @@ int main( int argc, char** argv ){
             draw();
             if(dtime >= 1000){
                 update(0);
-                //update(5);
-                draw();
+                draw();              // always update screen on ticks
                 dtime = 0;
             }else{
                 dtime = dtime + 1000/framerate;
             }
-            SDL_Delay(16);
+            SDL_Delay(1000/framerate);  // per frame interval in milisecs
             
     }
 
